5c3310224d6c955ab0e0260b880d2083f0290cee
janafrady <126286248+janafrady@users.noreply.github.com>
2024-10-05 19:19:28 -0500

    Add files via upload

--- code1.c
+++ code1.c
@@ -1,5 +1,3 @@
-// Football Score Possibilities
-
 /*
 Name: Jana Frady
 KUID: 3085051
@@ -8,7 +6,6 @@ Lab Assignment Number: 04
 Program Description: This program receives a score and determines all possible combinations of scoring plays that could result from the score.
 Collaborators: None
 */
-
 
 #include <stdio.h>
 
@@ -19,11 +16,8 @@ Collaborators: None
 #define field 3
 #define safety 2
 
-int calcScore(int safe, int fieldgoal, int touchd, int td_1pt, int td_2pt){
-    return td_2pt*TD2pt + td_1pt*TD1pt + touchd*TD + fieldgoal*field + safe*safety;
-}
-
 void combination(int score){
+    // keeps track of how many of each point value possible
     int safe = 0;
     int fieldgoal = 0;
     int touchd = 0;
@@ -31,45 +25,46 @@ void combination(int score){
     int td_2pt = 0;
 
     while(1){
-        if (calcScore(safe, fieldgoal, touchd, td_1pt, td_2pt) == score){
+        if (td_2pt*TD2pt + td_1pt*TD1pt + touchd*TD + fieldgoal*field + safe*safety == score){ //checks if score matches
             printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", td_2pt, td_1pt, touchd, fieldgoal, safe);
         }
 
         safe++;
-        if (safe*safety > score){
+        if (safe*safety > score){ // checks safety
             safe = 0;
             fieldgoal++;
         }
 
-        if (fieldgoal*field > score){
+        if (fieldgoal*field > score){ // checks fieldgoal
             fieldgoal = 0;
             touchd++;
         }
 
-        if (touchd*TD > score){
+        if (touchd*TD > score){ // checks touchdown
             touchd = 0;
             td_1pt++;
         }
 
-        if (td_1pt*TD1pt > score){
+        if (td_1pt*TD1pt > score){ // checks touchdown 1 point
             td_1pt = 0;
             td_2pt++;
         }
 
-        if (td_2pt*TD2pt > score){
+        if (td_2pt*TD2pt > score){ // checks touchdown 2 point
             break;
         }
     }
 }
 
 int main(){
+    // initializes program by asking user for input and checking to see if valid
     int score = 2;
     while (score != 0 || score != 1){
         int score;
         printf("\nEnter 0 or 1 to STOP\nEnter the NFL score: ");
         scanf("%d",&score);
         
-        if (score == 0 || score == 1){
+        if (score == 0 || score == 1){ // quits program
             printf("Goodbye\n");
             break;
         } else if (score < 0){
